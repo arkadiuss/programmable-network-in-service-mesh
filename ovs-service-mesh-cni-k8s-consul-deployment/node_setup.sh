15bf4631daa376d114d6769882a1d25ece324373
arkadiuss <arkadius.kraus@gmail.com>
2022-04-18 15:26:48 +0200

    working on bridge connection

--- ovs-service-mesh-cni-k8s-consul-deployment/node_setup.sh
+++ ovs-service-mesh-cni-k8s-consul-deployment/node_setup.sh
@@ -2,5 +2,6 @@
 
 OTHER_NODE_IP_ADDRESS=$1
 
-ovs-vsctl add-br br1
-ovs-vsctl add-port br1 vxlan -- set Interface vxlan type=vxlan options:remote_ip=$OTHER_NODE_IP_ADDRESS
+sudo ovs-vsctl add-br br1
+sudo ovs-vsctl add-port br1 vxlan -- set Interface vxlan type=vxlan options:remote_ip=$OTHER_NODE_IP_ADDRESS
+sudo ovs-vsctl set-controller br1 ptcp:6633
