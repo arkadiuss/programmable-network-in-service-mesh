472d0cf604a8cee5438ded252064c8daf6182a0b
arkadiuss <arkadius.kraus@gmail.com>
2022-06-05 14:44:36 +0200

    finish cluster networking, client-server deployment

--- performance-tests/ovs-sm/cluster.tf
+++ performance-tests/ovs-sm/cluster.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "k8s_cluster" {
   name                = "${local.prefix}-vmss"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  sku                 = "Standard_B2s"
+  sku                 = "Standard_B2ms"
   instances           = 2
   admin_username      = "adminuser"
   custom_data         = filebase64("./vm_init.yml")
